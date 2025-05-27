49c8cbb5cde9846bd55954c6c2a51a7897f90b44
ali yazdanifar <33312581+AliYazdanifar@users.noreply.github.com>
2025-05-27 16:05:00 +0330

    temp

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,11 +46,15 @@ class User extends Authenticatable
     ];
 
 
-    public function teams(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    public function teams(): BelongsToMany
     {
         return $this->belongsToMany(Team::class, 'team_user')
             ->withTimestamps();
     }
 
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 
 }
---
+++ database/migrations/2025_05_27_122305_create_permissionables_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissionables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('permission_id')->constrained('permissions')->onDelete('cascade');
+            $table->morphs('permissionable'); // This will create permissionable_id and permissionable_type columns
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissionables');
+    }
+};
